061da3854d2256422aef7fd23b8a15ad35bb5b6a
khanZhomart <jtalgatulu@yandex.kz>
2022-10-20 18:20:51 +0600

    Minor fixes

--- src/test/java/constants/Constants.java
+++ src/test/java/constants/Constants.java
@@ -8,7 +8,7 @@ public class Constants {
     public static final String USER_ID = "1575";
     public static final String USER_FULLNAME = "Zhomartkhan Talgatuly";
     public static final String USERNAME = "201670";
-    public static final String PASSWORD = "Jomatalg01#";
+    public static final String PASSWORD = "******";
 
     public static final String EXCEL_PATH = "src/test/resources/TestCase.xlsx";
     public static final String EXCEL_NAME = "TestCase.xlsx";
